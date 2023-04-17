e1ceae5bbe12ec14d0374f47aaa76e10097af934
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-17 17:32:56 +0530

    add configure code.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+    assetPrefix: isProd ? '' : '',
+    experimental: {
+        appDir: true,
+    },
+    images: {
+        unoptimized: true,
+    }
 }
 
 module.exports = nextConfig
